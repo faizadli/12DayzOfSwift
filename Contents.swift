9e6ab4460c2901943d9fd2136e3c39530ee49093
faizadli <mochfaizadli@gmail.com>
2021-03-16 13:37:49 +0700

    Hari petama - Variable, String, Multiline String

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
-var str = "Hello, playground"
+var nama = "Faiz"
+nama = "Adli"
+
+var umur = 15
+umur = 16
+
+var uang_jajan = 1_000_000
+
+var pantun = """
+  Buah rambutan
+  Buah mangga
+  Kamu serabutan
+  Saya bangga
+"""
+print(pantun)
